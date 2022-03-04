9853f3d019d67f98cd64017d88e98f1f843ac8c5
shvatov <ishvatov@gmail.com>
2022-03-04 19:04:08 +0300

    Analyzed the recursive function
    Signed-off-by: shvatov <ishvatov@gmail.com>

--- resources/index.js
+++ resources/index.js
@@ -8,3 +8,12 @@ function ternaryOperatorExample() {
       submittedAge < 18 ? 'no' : (submittedAge === 18 ? 'not sure' : 'yes');
   }
 }
+
+function recursionTest() {
+  const sec = a => 1 + a;
+  const add = (a, b) => (b === 0) ? a : sec(add(a, b - 1));
+  const mpy = (a, b) => (b === 1) ? a : add(a, mpy(a, b - 1));
+  const pwr = (a, b) => (b === 1) ? a : mpy(a, pwr(a, b - 1));
+
+  console.log(pwr(2, 4));
+}
